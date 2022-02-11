8f1e001ed77da57d1d1e6a3496bac674f00524a7
elariag12 <82555826+elariag12@users.noreply.github.com>
2022-02-10 22:54:16 -0500

    added part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Elaria Ghobrial on 2/9/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
